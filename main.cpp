f5d71acd43ed6368e5722e08f3e6b59ca25d6ebb
博奕 黄 <1853308@tongji.edu.cn>
2019-11-04 21:05:51 +0800

    Update main.cpp
    增加注释；适应关键字修改

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <forward_list>
+#include "struct.h"
 #include "D:\\MyLibrary\DefaultFunction.h"
 #include "D:\\MyLibrary\CustomizedRadixCharSet.h"
 
+//Caution:
+//	every constraint should be single.
 int main(int argc, char *argv[]) noexcept {
 	using namespace std;
 	using namespace Function;
@@ -18,10 +22,15 @@ int main(int argc, char *argv[]) noexcept {
 		for (int i = 1; i < argc; ++i) {
 			cout << "Trying to open " << argv[i] << '.' << endl;
 			ifstream fin(argv[i]);
+			(void)fin.peek();
 			if (!fin)return cerr << "Error in opening " << argv[i] << '.' << endl, -1;
-			if (fin.peek() == -1)return cerr << "Empty" << endl, -1;
 
+			//结点表 points map
+			forward_list<point> points;
 
+			//刚体表 bodies map
+			//基础（base）是具有不同性质的结点 basement is a body, though having different behaviours
+			forward_list<body> bodies = { "base" };
 
 			string words;
 			//per loop for per line
@@ -31,14 +40,11 @@ int main(int argc, char *argv[]) noexcept {
 				auto c = getline<' ', '\n', '\r'>(fin, words);
 				ignore_if<' '>(fin);
 				if (c == '\n' || c == '\r')break;
-				if (words.empty()) continue;
-				if (words == "point") {
-				}
-				else if (words == "body") {
 
-				}
-				else if (words=="pole") {
-
+				if (words == "pole") {
+					string name;
+					getline<' ', '\n', '\r'>(fin, name);
+					bodies.emplace_front(name);
 				}
 				else if (words == "lever") {
 
@@ -50,9 +56,12 @@ int main(int argc, char *argv[]) noexcept {
 
 				}
 				else cerr << "Unknown input \"" << words << "\"." << endl;
+
+
 				string res;
 				std::getline(fin, res);
 				if (!res.empty()) cerr << "Unused input \"" << res << "\"." << endl;
+
 			} while (words.clear(), !fin.eof());
 			cout << "Closing " << argv[i] << '.' << endl;
 			fin.close();
