c64def4af5826af41291610e1fc3836af9e3f98f
博奕 黄 <1853308@tongji.edu.cn>
2019-11-04 22:07:51 +0800

    结点、刚体表封装

--- main.cpp
+++ main.cpp
@@ -25,40 +25,51 @@ int main(int argc, char *argv[]) noexcept {
 			(void)fin.peek();
 			if (!fin)return cerr << "Error in opening " << argv[i] << '.' << endl, -1;
 
-			//结点表 points map
-			forward_list<point> points;
-
-			//刚体表 bodies map
-			//基础（base）是具有不同性质的结点 basement is a body, though having different behaviours
-			forward_list<body> bodies = { "base" };
-
 			string words;
 			//per loop for per line
 			do {
 				if (!fin) { cerr << "Error in reading " << argv[i] << '.' << endl; break; }
 				ignore_if<' '>(fin);
 				auto c = getline<' ', '\n', '\r'>(fin, words);
-				ignore_if<' '>(fin);
 				if (c == '\n' || c == '\r')break;
+
+				table t;
 
 				if (words == "pole") {
 					string name;
+					ignore_if<' '>(fin);
 					getline<' ', '\n', '\r'>(fin, name);
-					bodies.emplace_front(name);
+					t.bodies.emplace_front(name);
 				}
 				else if (words == "lever") {
-
+					string a, b;
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, a);
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, b);
+					t.points.emplace_front(a, b, joint::lever);
 				}
 				else if (words == "pin" || words == "hinge") {
-
+					string a, b;
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, a);
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, b);
+					t.points.emplace_front(a, b, joint::pin);
 				}
 				else if (words == "rigid") {
-
+					string a, b;
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, a);
+					ignore_if<' '>(fin);
+					getline<' ', '\n', '\r'>(fin, b);
+					t.points.emplace_front(a, b, joint::rigid);
 				}
 				else cerr << "Unknown input \"" << words << "\"." << endl;
 
 
 				string res;
+				ignore_if<' '>(fin);
 				std::getline(fin, res);
 				if (!res.empty()) cerr << "Unused input \"" << res << "\"." << endl;
 
--- struct.h
+++ struct.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <forward_list>
+#include <algorithm>
 
 class body {
 public:
@@ -7,11 +9,38 @@ public:
 	body(const std::string &that)noexcept :name(that) { }
 	body(const char *that)noexcept :name(that) { }
 	~body() { }
+	bool operator==(const std::string &str)noexcept { return this->name == str; }
+};
+
+enum class joint {
+	lever, pin, rigid
 };
 
 class point {
 public:
 	const body &a, &b;
+	const joint type;
+	point(const body &a, const body &b, const joint& joint_type)noexcept :a(a), b(b),type(joint_type) { }
+};
 
-	point(const body &a, const body &b)noexcept :a(a), b(b) { }
+class table {
+public:
+	//结点表 points map
+	std::forward_list<point> points;
+	//刚体表 bodies map
+	//基础（base）是具有不同性质的刚体 basement is a body, though having different behaviours
+	std::forward_list<body> bodies;
+	const body base = "base";
+
+	auto emplace_front(const std::string &name)noexcept {
+		return bodies.emplace_front(name);
+	}
+	auto emplace(const std::string &a, const std::string &b, const joint &joint_type)noexcept {
+		const body
+			&body1 = (a == "base") ? base : *std::find_if(bodies.begin(), bodies.end(), [&a](auto& _body) { return a == _body; }),
+			&body2 = (b == "base") ? base : *std::find_if(bodies.begin(), bodies.end(), [&b](auto& _body) { return b == _body; });
+		return points.emplace_front(body1, body2, joint_type);
+	}
+
+private:
 };
