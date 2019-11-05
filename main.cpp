25e924b90ed987e860579437915c8d58a2fd1e51
博奕 黄 <1853308@tongji.edu.cn>
2019-11-05 21:46:04 +0800

    测试文件语法修正(correction on grammar to test file)；结构和约束的完善(perfection about structure and constraint)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 //Caution:
 //	every constraint should be single.
 int main(int argc, char *argv[]) noexcept {
+#ifdef _MSVC_LANG
+	_CrtSetDbgFlag(_CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_EVERY_16_DF);
+#endif // _MSVC_LANG
 	using namespace std;
 	using namespace Function;
 	using namespace LargeInteger;
@@ -23,6 +26,7 @@ int main(int argc, char *argv[]) noexcept {
 			ifstream fin(argv[i]);
 			(void)fin.peek();
 			if (!fin)return cerr << "Error in opening " << argv[i] << '.' << endl, -1;
+			table t;
 
 			string words;
 			//per loop for per line
@@ -32,37 +36,68 @@ int main(int argc, char *argv[]) noexcept {
 				auto c = getline<' ', '\n', '\r'>(fin, words);
 				if (c == '\n' || c == '\r')break;
 
-				table t;
-
+				joint type = joint::unknown;
 				if (words == "pole") {
-					string name;
+					string name, x, y, begin, end;
 					ignore_if<' '>(fin);
 					getline<' ', '\n', '\r'>(fin, name);
-					t.bodies.emplace_front(name);
+
+					ignore_if_not<'x'>(fin);
+					getline<'y', '\n', '\r'>(fin, x);
+
+					ignore_if_not<'y'>(fin);
+					getline<'('>(fin, y);
+
+					ignore_if<'(', '=', ' ', ','>(fin);
+					getline<',', ' ', '\n', '\r'>(fin, begin);
+					getline<'\n', '\r'>(fin, end);
+
+
+					t.emplace_body(name.c_str(), x.c_str(), y.c_str());
 				}
-				else if (words == "lever") {
-					string a, b;
+				else if (
+					(type = joint::lever, words == "lever")
+					||
+					(type = joint::pin, words == "pin" || words == "hinge")
+					||
+					(type = joint::rigid, words == "rigid")
+					) {
+					string a, b, x, y;
 					ignore_if<' '>(fin);
 					getline<' ', '\n', '\r'>(fin, a);
 					ignore_if<' '>(fin);
 					getline<' ', '\n', '\r'>(fin, b);
-					t.points.emplace_front(a, b, joint::lever);
-				}
-				else if (words == "pin" || words == "hinge") {
-					string a, b;
 					ignore_if<' '>(fin);
-					getline<' ', '\n', '\r'>(fin, a);
+					getline<' ', '\n', '\r'>(fin, x);
 					ignore_if<' '>(fin);
-					getline<' ', '\n', '\r'>(fin, b);
-					t.points.emplace_front(a, b, joint::pin);
-				}
-				else if (words == "rigid") {
-					string a, b;
-					ignore_if<' '>(fin);
-					getline<' ', '\n', '\r'>(fin, a);
-					ignore_if<' '>(fin);
-					getline<' ', '\n', '\r'>(fin, b);
-					t.points.emplace_front(a, b, joint::rigid);
+					getline<' ', '\n', '\r'>(fin, y);
+					switch (type) {
+					case joint::lever:
+					{
+						string angle;
+						ignore_if<' '>(fin);
+						getline<' ', '\n', '\r'>(fin, angle);
+						t.emplace_constraint(a, b, x, y, atol(angle.c_str()), true);
+					}
+					break;
+					case joint::pin:
+					{
+						t.emplace_constraint(a, b, x, y, 0, true);
+						t.emplace_constraint(a, b, x, y, 90, true);
+					}
+					break;
+					case joint::rigid:
+					{
+						t.emplace_constraint(a, b, x, y, 0, true);
+						t.emplace_constraint(a, b, x, y, 90, true);
+						t.emplace_constraint(a, b, x, y, 90, false);
+					}
+					break;
+					case joint::unknown:
+					default:
+						cerr << "Unknown joint type" << endl;
+						break;
+					}
 				}
 				else cerr << "Unknown input \"" << words << "\"." << endl;
 
--- struct.h
+++ struct.h
@@ -7,40 +7,62 @@
 class body {
 public:
 	const std::string name;
-	body(const std::string &that)noexcept :name(that) { }
-	body(const char *that)noexcept :name(that) { }
-	~body() { }
+	ptrHolder x, y;
+	body(const std::string &that, function *x, function *y)noexcept :name(that), x(x), y(y) { }
+	body(const std::string &that, ptrHolder &&x, ptrHolder &&y)noexcept
+		:name(that), x(std::move(x)), y(std::move(y)) { }
+	body(const char *that, function *x, function *y)noexcept :name(that), x(x), y(y) { }
 	bool operator==(const std::string &str)const noexcept { return this->name == str; }
 };
 
 enum class joint {
-	lever, pin, rigid
+	unknown = -1, lever, pin, rigid
 };
 
-class point {
+using ang = size_t;
+using co = size_t;
+
+class constraint {
 public:
 	const body &a, &b;
-	const joint type;
-	point(const body &a, const body &b, const joint& joint_type)noexcept :a(a), b(b),type(joint_type) { }
+	co x, y;
+	bool force_or_moment;
+	ang direction;
+	constraint(const body &a, const body &b, co x, co y, ang angle, bool force_or_moment)noexcept
+		:a(a), b(b), x(x), y(y), direction(angle), force_or_moment(force_or_moment) { }
 };
 
 class table {
 public:
 	//结点表 points map
-	std::forward_list<point> points;
+	std::forward_list<constraint> constraints;
 	//刚体表 bodies map
 	//基础（base）是具有不同性质的刚体 basement is a body, though having different behaviours
 	std::forward_list<body> bodies;
-	const body base = "base";
+	const body base = body("base", nullptr, nullptr);
 
-	auto emplace_front(const std::string &name)noexcept {
-		return bodies.emplace_front(name);
+	void emplace_body(const char*name, const char* x, const char* y)noexcept {
+		try {
+			auto &&fun_x = funEngine::produce(x), &&fun_y = funEngine::produce(y);
+			bodies.emplace_front(name, std::move(fun_x), std::move(fun_y));
+		}
+		catch (const std::exception & e) {
+			std::cerr
+				<< "Exception:\"" << e.what() << "\"" << std::endl
+				<< "It may be caused by unsupported form of equation \"" << x
+				<< "\" and \"" << y << "\"."
+				<< std::endl;
+		}
 	}
-	auto emplace(const std::string &a, const std::string &b, const joint &joint_type)noexcept {
+	void emplace_constraint(
+		const std::string &a, const std::string &b,
+		const std::string &x, const std::string &y,
+		ang angle, bool force_or_moment
+	)noexcept {
 		const body
-			&body1 = (a == "base") ? base : *std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto& _body) { return a == _body; }),
-			&body2 = (b == "base") ? base : *std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto& _body) { return b == _body; });
-		return points.emplace_front(body1, body2, joint_type);
+			&body1 = (a == "base") ? base : *std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto &_body) { return a == _body; }),
+			&body2 = (b == "base") ? base : *std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto &_body) { return b == _body; });
+		constraints.emplace_front(body1, body2, std::atol(x.c_str()), std::atol(y.c_str()), angle, force_or_moment);
 	}
 
 private:
