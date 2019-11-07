c2fbb0c78482d53612b976f11741d1fd608df673
博奕 黄 <1853308@tongji.edu.cn>
2019-11-07 12:35:09 +0800

    精确性提高 increase the precision

--- struct.h
+++ struct.h
@@ -6,6 +6,8 @@
 #include "D:\\MyLibrary\DefaultFunction.h"
 #include "D:\\MyLibrary\NormalMatrix.h"
 
+using namespace Darkness;
+
 class body {
 public:
 	const std::string name;
@@ -22,7 +24,7 @@ enum class joint {
 };
 
 using ang = size_t;
-using co = size_t;
+using co = constant;
 using key = size_t;
 
 class constraint {
@@ -38,15 +40,15 @@ public:
 class table {
 	key countBody = 0;
 	key countConstr = 0;
-	//结点表 points map
-	std::forward_list<constraint> constraints;
-	//刚体表 bodies map
-	//基础（base）是具有不同性质的刚体 basement is a body, though having different behaviours
+	//刚体表 rigid bodies map
+	//基础（base）是具有不同性质的刚体 basement is a rigid body, though having different behaviours
 	std::map<key, body> bodies;
+	//约束表 constraints map
+	std::forward_list<constraint> constraints;
 public:
-	const body base = body("base", nullptr, nullptr);
+	//const body base = body("base", nullptr, nullptr);
 	__stdcall table() = default;
-	__stdcall table(const table &that)noexcept = default;
+	__stdcall table(const table &that) = default;
 
 	void emplace_body(const char *name, const char *x, const char *y)noexcept {
 		try {
@@ -68,48 +70,55 @@ public:
 		ang angle, bool force_or_moment
 	)noexcept {
 		constraints.emplace_front(
-			(a == "base") ? key(0) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto &_body) { return a == _body.second; })).first,
-			(b == "base") ? key(0) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto &_body) { return b == _body.second; })).first,
-			std::atol(x.c_str()), std::atol(y.c_str()),
+			(a == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto &_body) { return a == _body.second; })).first,
+			(b == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto &_body) { return b == _body.second; })).first,
+			x.c_str(), y.c_str(),
 			angle, force_or_moment
 		);
 		++countConstr;
 	}
 	void solve()noexcept {
-		Math::NormalMatrix<double> constr(countBody * 3, countConstr + 1);
-		constr.fill([this](size_t i, size_t j) ->double {
+		Math::NormalMatrix<constant> constr(countBody * 3, countConstr + 1);
+		constr.fill([this](size_t i, size_t j) ->constant {
+			//第(i / 3)个刚体，第j个约束
+			//Please try Google Translation this time.
 			auto iter = constraints.cbegin();
-			if (j >= countConstr)return 0;
+			if (j >= countConstr)return constant(true, 0, 1);
 			for (size_t t = 0; t < j; t++) {
 				++iter;
 			}
+			//由于a b所受约束力方向相反，需要判断
+			//As the direction of the constrain force to a and b is contrary, a variable should be used.
 			bool is_a = true;
 			if ((i / 3) == (*iter).a || (is_a = false, (i / 3) == (*iter).b)) {
 				if ((*iter).force_or_moment == true) {
 					if (i % 3 == 0) {
-						return cos((*iter).direction * PI / 180) * (is_a ? 1 : -1);
+						auto &&res = cosd((*iter).direction);
+						return  (is_a ? res : -res);
 					}
-					if (i % 3 == 1) {
-						return sin((*iter).direction * PI / 180) * (is_a ? 1 : -1);
+					else if (i % 3 == 1) {
+						auto &&res = sind((*iter).direction);
+						return  (is_a ? res : -res);
 					}
-					if (i % 3 == 2) {
-						return ((*iter).y * sin((*iter).direction * PI / 180) - (*iter).x * cos((*iter).direction * PI / 180)) * (is_a ? 1 : -1);
+					else if (i % 3 == 2) {
+						auto &&res = ((*iter).x * sind((*iter).direction) - (*iter).y * cosd((*iter).direction));
+						return (is_a ? res : -res);
 					}
 					assert(false);
-					return NAN;
+					return constant(true, 0, 0);
 				}
 				else {
 					if (i % 3 == 2) {
-						return (is_a ? 1 : -1);
+						return constant(is_a, 1, 1);
 					}
-					else return 0;
+					else return constant(true, 0, 1);
 				}
 			}
-			else return 0;
+			else return constant(true, 0, 1);
 			});
-		std::cout << constr << std::endl;
+		std::cout << "Before solving:" << std::endl << constr << std::endl;
 		constr.to_diagon();
-		std::cout << constr << std::endl;
+		std::cout << "After solving:" << std::endl << constr << std::endl;
 	}
 private:
 };
