1e3f5bee2cbfb374452edf441c8802ea2b77e8c6
博奕 黄 <1853308@tongji.edu.cn>
2019-11-07 17:11:33 +0800

    异常处理deal with unexpected input

--- struct.h
+++ struct.h
@@ -69,56 +69,90 @@ public:
 		const std::string &x, const std::string &y,
 		ang angle, bool force_or_moment
 	)noexcept {
-		constraints.emplace_front(
-			(a == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto &_body) { return a == _body.second; })).first,
-			(b == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto &_body) { return b == _body.second; })).first,
-			x.c_str(), y.c_str(),
-			angle, force_or_moment
-		);
-		++countConstr;
+		try {
+			constraints.emplace_front(
+				(a == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&a](auto &_body) { return a == _body.second; })).first,
+				(b == "base") ? key(-1) : (*std::find_if(bodies.cbegin(), bodies.cend(), [&b](auto &_body) { return b == _body.second; })).first,
+				x.c_str(), y.c_str(),
+				angle, force_or_moment
+			);
+			++countConstr;
+		}
+		catch (const std::exception & e) {
+			std::cerr
+				<< "Exception:\"" << e.what() << "\"" << std::endl
+				<< std::endl;
+		}
 	}
-	void solve()noexcept {
-		Math::NormalMatrix<constant> constr(countBody * 3, countConstr + 1);
-		constr.fill([this](size_t i, size_t j) ->constant {
-			//第(i / 3)个刚体，第j个约束
-			//Please try Google Translation this time.
-			auto iter = constraints.cbegin();
-			if (j >= countConstr)return constant(true, 0, 1);
-			for (size_t t = 0; t < j; t++) {
-				++iter;
-			}
-			//由于a b所受约束力方向相反，需要判断
-			//As the direction of the constrain force to a and b is contrary, a variable should be used.
-			bool is_a = true;
-			if ((i / 3) == (*iter).a || (is_a = false, (i / 3) == (*iter).b)) {
-				if ((*iter).force_or_moment == true) {
-					if (i % 3 == 0) {
-						auto &&res = cosd((*iter).direction);
-						return  (is_a ? res : -res);
+	void solve() noexcept {
+		try {
+			Math::NormalMatrix<constant> constr(countBody * 3, countConstr + 1);
+			constr.fill([this](size_t i, size_t j) ->constant {
+				try {
+					//第(i / 3)个刚体，第(countConstr - j - 1)个约束
+					//Please try Google Translation this time.
+					auto iter = constraints.cbegin();
+					if (j >= countConstr)return constant(true, 0, 1);
+					for (size_t t = 0; t < j; t++) {
+						++iter;
 					}
-					else if (i % 3 == 1) {
-						auto &&res = sind((*iter).direction);
-						return  (is_a ? res : -res);
-					}
-					else if (i % 3 == 2) {
-						auto &&res = ((*iter).x * sind((*iter).direction) - (*iter).y * cosd((*iter).direction));
-						return (is_a ? res : -res);
-					}
-					assert(false);
-					return constant(true, 0, 0);
-				}
-				else {
-					if (i % 3 == 2) {
-						return constant(is_a, 1, 1);
+					//由于a b所受约束力方向相反，需要判断
+					//As the direction of the constrain force to a and b is contrary, a variable should be used.
+					bool is_a = true;
+					if ((i / 3) == (*iter).a || (is_a = false, (i / 3) == (*iter).b)) {
+						if ((*iter).force_or_moment == true) {
+							if (i % 3 == 0) {
+								auto &&res = cosd((*iter).direction);
+								return  (is_a ? res : -res);
+							}
+							else if (i % 3 == 1) {
+								auto &&res = sind((*iter).direction);
+								return  (is_a ? res : -res);
+							}
+							else if (i % 3 == 2) {
+								auto &&res = ((*iter).x * sind((*iter).direction) - (*iter).y * cosd((*iter).direction));
+								return (is_a ? res : -res);
+							}
+							assert(false);
+							return constant(true, 0, 0);
+						}
+						else {
+							if (i % 3 == 2) {
+								return constant(is_a, 1, 1);
+							}
+							else return constant(true, 0, 1);
+						}
 					}
 					else return constant(true, 0, 1);
 				}
+				catch (const std::exception & e) {
+					std::cerr
+						<< "Exception:\"" << e.what() << "\"" << std::endl
+						<< "It occured at (" << i
+						<< ", " << j << ")."
+						<< std::endl;
+					throw std::exception("Failed in filling the matrix.");
+				}
+				});
+			std::cout << "Before solving:" << std::endl << constr << std::endl;
+			auto rank = constr.to_diagon();
+			std::cout << "After solving:" << std::endl << constr << std::endl;
+			if (rank < countBody * 3) {
+				std::cout << "Variable structure!It has " << countBody * 3 - rank << " free degree." << std::endl;
 			}
-			else return constant(true, 0, 1);
-			});
-		std::cout << "Before solving:" << std::endl << constr << std::endl;
-		constr.to_diagon();
-		std::cout << "After solving:" << std::endl << constr << std::endl;
+			else if (countBody * 3 == countConstr) {
+				std::cout << "Statically determinate structure!" << std::endl;
+			}
+			else {
+				std::cout << "Statically indeterminate structure!Its degree of statical indeterminacy is " << countConstr - countBody * 3 << '.' << std::endl;
+			}
+
+		}
+		catch (const std::exception & e) {
+			std::cerr
+				<< "Exception:\"" << e.what() << "\"" << std::endl
+				<< std::endl;
+		}
 	}
 private:
 };
